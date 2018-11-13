7b8766fc7df6e6be0b31ca357fe02f8e17d98c16
Nikhil Soni <nikhilkamlesh.soni@gmail.com>
2018-11-13 12:02:19 -0500

    init

---
+++ HW6.R
@@ -0,0 +1,4 @@
+save(list=ls(all=T), file="HW6.RData")
+# header----
+setwd("~/Google Drive/Purdue University/Academics/Sem-3/STAT545/HW6")
+load("~/Google Drive/Purdue University/Academics/Sem-3/STAT545/HW6/HW6.RData")
