abb5dd7ef0dd5fbf86b6831625d5c776880f4176
Nikhil Soni <nikhilkamlesh.soni@gmail.com>
2018-11-14 18:29:31 -0500

    shannon entropy

--- HW6.R
+++ HW6.R
@@ -4,17 +4,24 @@ setwd("~/Google Drive/Purdue University/Academics/Sem-3/STAT545/HW6")
 load("~/Google Drive/Purdue University/Academics/Sem-3/STAT545/HW6/HW6.RData")
 options(scipen=5)
 
+# functions----
+shannon_entropy<-function(vec)
+{
+  freqs<-table(vec)/length(vec)
+  return(-sum(freqs * log2(freqs)))
+}
 
 # data----
 wp<-readChar("input/wp.txt", file.info("input/wp.txt")$size)
 wp<-gsub("\r", "", wp)
 wp<-gsub("\n", " ", wp)
 wp<-gsub("\\s{2,100}", " ", wp)
-wp<-gsub("[^a-z\\,\\.\\: ]", "", wp)
 wp<-tolower(wp)
+wp<-gsub("[^a-z\\,\\.\\: ]", "", wp)
 wp<-unlist(strsplit(wp, ""))
 wp.freq<-t(as.matrix(table(wp)))
 
 # operations----
 
 barplot(wp.freq, main="Histogram of frequency")
+shannon_entropy(wp)
