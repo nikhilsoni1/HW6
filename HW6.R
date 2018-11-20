5729f6b933db149b75b58a584275eb6561aa4f46
diadtp <38264815+diadtp@users.noreply.github.com>
2018-11-19 19:28:38 -0500

    Update HW6.R

--- HW6.R
+++ HW6.R
@@ -1,6 +1,7 @@
 save(list=ls(all=T),  file="HW6.RData")
 # header----
 setwd("~/Google Drive/Purdue University/Academics/Sem-3/STAT545/HW6")
+setwd("C:/Users/manan/OneDrive - purdue.edu/Sem 3/Stat545/HW6")
 load("~/Google Drive/Purdue University/Academics/Sem-3/STAT545/HW6/HW6.RData")
 options(scipen=5)
 
