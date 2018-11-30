93d45078e0a2c413773721e29da02254502a4b8c
Nikhil Soni <nikhilkamlesh.soni@gmail.com>
2018-11-29 19:23:44 -0500

    Revert "Working Code: Eureka!"
    This reverts commit f4dc8823b8a9f8519532ba525ec6acac3704cc52.

--- HW6.R
+++ HW6.R
@@ -26,58 +26,32 @@ acceptance<-function(T.mat, X, perm)
   acc<-min(1, (a$ll/b$ll))
   return(list(new_perm=rand_perm, new_msg=a$msg, old_msg=b$msg, old_perm=perm, acc=acc))
 }
-decode<-function(K, msg)
-{
-  Y<-list()
-  for(i in 1:length(msg))
-  {
-    Y<-c(Y,unlist(K[[msg[i]]]))
-  }
-  return(unlist(Y))
-}
-encode<-function(K, msg, iter=1000)
-{
-  for(i in 1:iter)
-  {
-    K<-random_perm(K)
-  }
-  msg<-unlist(strsplit(msg, ""))
-  a<-char_map(K, msg)
-  return(list(perm=K, msg=a, orig=msg))
-}
-MHalgo<-function(T.mat, X, perm, run=3000, verbose=F, B=1000)
+MHalgo<-function(T.mat, X, perm, run=3000, verbose=F)
 {
   CTR<-1
+  message<-""
   while(CTR<=run)
   {
     FLAG<-F
     acc<-acceptance(T.mat, X, perm)
-    if(CTR>B)
+    if(acc$acc<1)
     {
-      CTR<-1
-      if(acc$acc<1)
-      {
-        perm = acc$old_perm
-        FLAG<-T
-      }
-      if((CTR %% 100)==0 && verbose)
-      {
-        print(CTR)
-        if(FLAG)
-        {
-          print(paste(acc$old_msg[1:20], collapse = ''))
-        }
-        else
-        {
-          print(paste(acc$new_msg[1:20], collapse = ''))
-        }
-      }
-    }
-    else
-    {
-      perm<-acc$new_perm
+      perm = acc$new_perm
+      FLAG<-T
     }
     CTR<-CTR+1
+    if((CTR %% 100)==0 && verbose)
+    {
+      print(CTR)
+      if(FLAG)
+      {
+        print(paste(acc$new_msg[1:20], collapse = ''))
+      }
+      else
+      {
+        print(paste(acc$old_msg[1:20], collapse = ''))
+      }
+    }
   }
 }
 likelihood<-function(T.mat, X, perm)
@@ -85,7 +59,12 @@ likelihood<-function(T.mat, X, perm)
   
   X<-gsub("\n", "", X)
   X<-unlist(strsplit(X, ""))
-  X<-decode(perm, X)
+  Y<-list()
+  for(i in 1:length(X))
+  {
+    Y<-c(Y,unlist(perm[[X[i]]]))
+  }
+  X<-unlist(Y)
   diff<-setdiff(symbol, X)
   freq.X<-as.matrix(table(X[1:length(X)-1],X[2:length(X)]))
   rn<-rownames(freq.X)
@@ -115,9 +94,9 @@ wp<-tolower(wp)
 wp<-gsub("[^a-z\\,\\.\\: ]", "", wp)
 wp<-unlist(strsplit(wp, ""))
 wp.freq<-t(as.matrix(table(wp)))
-T.mat<-unclass(table(wp[1:length(wp)-1],wp[2:length(wp)]))
+T.mat<-as.matrix(table(wp[1:length(wp)-1],wp[2:length(wp)]))
 T.mat[T.mat==0]<-1e-03
-T.mat<-t(apply(T.mat, 1, function(x) x/sum(x)))
+T.mat<-apply(T.mat, 1, function(x) x/sum(x))
 symbol<-sort(unique(wp))
 perm<-setNames(as.list(symbol), symbol)
 
@@ -125,4 +104,4 @@ perm<-setNames(as.list(symbol), symbol)
 barplot(wp.freq, main="Histogram of frequency")
 shannon_entropy(wp)
 heatmap(T.mat, Colv = NA, Rowv = NA, scale = "column")
-MHalgo(T.mat, msg, perm, verbose=T, run=10000)
+MHalgo(T.mat, msg, perm, verbose=T)
